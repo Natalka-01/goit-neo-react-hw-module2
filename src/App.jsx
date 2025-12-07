951026c2bc0a4cf1cc94978c08fc631ccb5d6a33
Natalka-01 <nataliia.dubikova@gmail.com>
2025-12-07 18:53:27 +0100

    fixed buttons03

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.module.css";
+import styles from "./App.module.css";
 import Description from "./components/Description/Description";
 import Feedback from "./components/Feedback/Feedback";
 import Options from "./components/Options/Options";
@@ -55,11 +55,11 @@ const avgFeedback = Math.round((values.good/totalFeedback) * 100)
 
 
   return (
-    <>
+    <div className={styles.app}>
       <Description />
       <Options onUpdate={updateFeedback} onReset={resetFeedback} total={totalFeedback}/>
       {totalFeedback === 0 ? ( <Notification />) : <Feedback good = {values.good} neutral={values.neutral} bad = {values.bad} total={totalFeedback} avg = {avgFeedback}/> }     
-    </>
+    </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./App.module.css"
+import styles from "./App.module.css"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
