2868532754774268c897c0a7633d7b9bdb38b613
Natalka-01 <nataliia.dubikova@gmail.com>
2025-12-07 19:14:50 +0100

    fixed buttons05

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import styles from "./App.module.css";
+import "./App.module.css";
 import Description from "./components/Description/Description";
 import Feedback from "./components/Feedback/Feedback";
 import Options from "./components/Options/Options";
@@ -55,7 +55,7 @@ const avgFeedback = Math.round((values.good/totalFeedback) * 100)
 
 
   return (
-    <div className={styles.app}>
+    <div className="app">
       <Description />
       <Options onUpdate={updateFeedback} onReset={resetFeedback} total={totalFeedback}/>
       {totalFeedback === 0 ? ( <Notification />) : <Feedback good = {values.good} neutral={values.neutral} bad = {values.bad} total={totalFeedback} avg = {avgFeedback}/> }     
